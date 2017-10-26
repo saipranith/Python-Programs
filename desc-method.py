373204b3d45c94d0b34a443fda9ed3cbf3d2b252
saipranith <saipranith@users.noreply.github.com>
2017-10-26 10:06:53 +0530

    Initial commit

---
+++ desc-method.py
@@ -0,0 +1,30 @@
+class Animal(object):
+    """Makes cute animals."""
+    is_alive = True
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    # Add your method here!
+    def description(self):
+        print self.name
+        print self.age
+
+hippo = Animal('yoyo', 20)
+print hippo.description()
+
+""" or 
+class Animal(object):
+    """Makes cute animals."""
+    is_alive = True
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    # Add your method here!
+    def description(Animal):
+        print Animal.name
+        print Animal.age
+
+hippo = Animal('yoyo', 20)
+print hippo.description()
+
+"""
